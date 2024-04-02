a35a134b47aa69609bdbfcc7cdc7f8c66541eac3
wjc <wangjianchengcs@163.com>
2024-04-02 15:50:42 +0800

    修复CHECK_NCCL宏函数由于文件末尾缺少换行符而导致的警告

--- include/qcu_macro.cuh
+++ include/qcu_macro.cuh
@@ -59,11 +59,11 @@ END_NAMESPACE(qcu)
     }                                                                                              \
   } while (0)
 
-#define CHECK_NCCL(cmd)                                                                             \
+#define CHECK_NCCL(cmd)                                                                            \
   do {                                                                                             \
     ncclResult_t err = cmd;                                                                        \
     if (err != ncclSuccess) {                                                                      \
-      fprintf(stderr, "NCCL error: %s\n", ncclGetErrorString(err));                                 \
+      fprintf(stderr, "NCCL error: %s\n", ncclGetErrorString(err));                                \
       exit(1);                                                                                     \
     }                                                                                              \
   } while (0)
